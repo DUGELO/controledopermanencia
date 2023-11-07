8767bbbd202ba9c4a1a35e1379dd2144755eecc1
Eduardo Ângelo <118673855+DUGELO@users.noreply.github.com>
2023-11-07 05:01:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+var autoridadesExistem = [];
+
 function changeArrow(imgId, direction) {
     // função para mudar a seta
     var arrow = document.getElementById(imgId);
+    arrow.classList.remove("seta");
     if (direction === "direita") {
         arrow.src = "direita.png";
     } else if (direction === "esquerda") {
@@ -17,24 +20,49 @@ function showForm() {
     autoridadeForm.style.display = 'block';
     formContainer.style.display = 'none';
 }
-
+                
 function addAutoridade() {
-    // código para adicionar autoridade   
     var nomeAutoridade = document.getElementById('nome-autoridade').value;
 
-    // prosseguir com a adição da nova autoridade
-    var novoCodigoHTML = '<span><h3>' + nomeAutoridade + ' <img id="' + nomeAutoridade + '-arrow\"></h3><button class="button1" data-direction="esquerda" onclick="changeArrow(\'' + nomeAutoridade + '-arrow\', this.getAttribute(\'data-direction\'))">Esquerda</button1><button class="button1" data-direction="direita" onclick="changeArrow(\'' + nomeAutoridade + '-arrow\', this.getAttribute(\'data-direction\'))">Direita</button><button class="button1" data-direction="desceu" onclick="changeArrow(\'' + nomeAutoridade + '-arrow\', this.getAttribute(\'data-direction\'))">Desceu</button><button class="buttondel" onclick="excluirAutoridade()">Excluir</button></span>';
-    document.getElementById('pai').insertAdjacentHTML('beforeend', novoCodigoHTML);
+        // Verifique se o nome já existe no array
+        if (autoridadesExistem.includes(nomeAutoridade)) {
+            alert('Este nome de autoridade já foi adicionado.');
+            return; // Não permita adicionar autoridades com o mesmo nome
+        }
+    
+        // Adicione o nome à lista de autoridades existentes
+        autoridadesExistem.push(nomeAutoridade);
 
-    // redefinir o formulário
-    document.getElementById("nome-autoridade").value = "";
+    // Verificar se o campo de nome está vazio
+    if (nomeAutoridade.trim() === "") {
+        alert("Por favor, insira o nome da autoridade antes de adicionar.");
+        return; // Não adiciona se o nome estiver vazio
+    }
 
-    // exibir o botão de adicionar novamente
-    document.getElementById("form-container").style.display = "block";
+    // Crie um novo elemento <span> para a autoridade
+    var novoSpan = document.createElement('span');
+    novoSpan.className = 'a1'; // Aplicar a classe 'a1' ao novo span
 
-    // ocultar o formulário
-    document.getElementById("autoridade-form").style.display = "none";
+    // Conteúdo dentro do novo span
+    novoSpan.innerHTML = `
+        <img class='fotos' src="homem-militar.png" alt="${nomeAutoridade}"><h3 class="nome1"> ${nomeAutoridade} </h3>
+        <div class="div_btn">
+            <button class="button1" data-direction="esquerda" onclick="changeArrow('${nomeAutoridade}-arrow', 'esquerda')">Esquerda</button>
+            <button class="button1" data-direction="direita" onclick="changeArrow('${nomeAutoridade}-arrow', 'direita')">Direita</button>
+            <button class="button1" data-direction="desceu" onclick="changeArrow('${nomeAutoridade}-arrow', 'desceu')">Desceu</button>
+            <button class="buttondel button1" onclick="excluirAutoridade()">Excluir</button>
+        </div>
+        <img class='seta  visitante-arrow' id="${nomeAutoridade}-arrow">`;
+
+    // Adicione o novo span ao contêiner 'one'
+    document.getElementById('one').appendChild(novoSpan);
+
+    // Redefina o formulário e exiba o botão de adicionar novamente
+    document.getElementById('nome-autoridade').value = '';
+    document.getElementById('form-container').style.display = 'block';
+    document.getElementById('autoridade-form').style.display = 'none';
 }
+
 
 function cancelar() {
     // redefinir o formulário
@@ -49,7 +77,7 @@ function cancelar() {
 
 function excluirAutoridade() {
     // função para excluir a autoridade adicionada
-    const container = document.getElementById('pai');
+    const container = document.getElementById('one');
     const lastChild = container.lastElementChild;
     container.removeChild(lastChild);
 }
